08b9a49cbaa37a0c30911d1d97abd5b25049f643
orsicn <orsicn@oregonstate.edu>
2026-02-11 15:55:56 -0800

    Add Leaflet Library

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-// Add all scripts to the JS folder
+var map = L.map('map').setView([40, -95], 4);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+ //Add all scripts to the JS folder
